e922060fd82d447b159622266ba98c9b7fa4f10e
ezelu <ezelujhay@gmail.com>
2022-08-04 14:22:09 -0700

    Feat: Added loader and fixed http error

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -32,169 +32,19 @@ export const fetchWeatherForecast = async (lat, lon) => {
 
 }
 
-// export const fetchWeatherForecast = async (lat, lon) => {
-//   const URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7000c2331d3aa19eefcaf49a1b99815d`;
-  
-//   const {data} = await axios.get(URL);
-  
-//   return data;
-
-// }
 
 
 
 
 export const CONVERT_LOCATION = async (query) => {
-  const URL = `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=7000c2331d3aa19eefcaf49a1b99815d`
+  const URL = `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=7000c2331d3aa19eefcaf49a1b99815d`
   
   const { data } = await axios.get(URL);
 
   return {
     lat: data[0].lat,
-    lon: data[0].lon
+    lon: data[0].lon,
   };
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import axios from 'axios';
-
-
-
-// const API_KEY = '7000c2331d3aa19eefcaf49a1b99815d';
-
-
-
-
-// export const fetchWeather = async (lat, lon) => {
-//   const URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7000c2331d3aa19eefcaf49a1b99815d`;
-  
-//   const {data} = await axios.get(URL);
-  
-//   return data;
-
-// }
-
-
-
-
-// export const CONVERT_LOCATION = async (query) => {
-//   const URL = `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=7000c2331d3aa19eefcaf49a1b99815d`
-  
-//   const { data } = await axios.get(URL);
-
-//   return {
-//     lat: data[0].lat,
-//     lon: data[0].lon
-//   };
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import axios from 'axios';
-
-
-
-
-// // const URL = 'https://api.openweathermap.org/data/2.5/weather';
-// const URL = 'https://api.openweathermap.org/data/2.5/forecast/hourly';
-// const API_KEY = '7000c2331d3aa19eefcaf49a1b99815d';
-
-
-// export const fetchWeather = async (query) => {
-//   const {data} = await axios.get(URL, {
-//     params: {
-//       q: query,
-//       units: 'metric',
-//       APPID: API_KEY,
-//     }
-//   });
-
-//   return data;
-// }
 
